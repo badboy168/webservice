cf01973b08f1c6cf2ad83d334c9b3c39afd036f9
longjiang <375638309@qq.com>
2017-12-12 15:07:41 +0800

    modify sms send args

--- public/index.php
+++ public/index.php
@@ -8,9 +8,11 @@
  */
 
 
-//如果需要设置允许所有域名发起的跨域请求，可以使用通配符 *
-header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求
-header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With');
+//如果需要设置允许所有域名发起的跨域请求，可以使用通配符
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
+header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+header('Access-Control-Allow-Credentials', 'true');
 
 
 
