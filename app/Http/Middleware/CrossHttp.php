7af22ac4f2b5831aab4ce64209acce4e94495b7e
longjiang <375638309@qq.com>
2017-12-12 17:49:46 +0800

    add .*

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -18,16 +18,11 @@ class CrossHttp
 
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
-//        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
+//        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Access-Token, Cookie, Accept, multipart/form-data, application/json');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'false');
-
-
-//        header('Access-Control-Allow-Origin: *');
-//        header("Access-Control-Allow-Credentials: true");
-//        header("Access-Control-Allow-Methods: *");
-//        header("Access-Control-Allow-Headers: Content-Type,Access-Token");
-//        header("Access-Control-Expose-Headers: *");
+        
 
         return $response;
     }
