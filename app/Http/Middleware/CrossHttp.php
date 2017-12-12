3e69329deab87d87b53e0e1fc57bb4728b188d9b
longjiang <375638309@qq.com>
2017-12-12 18:22:01 +0800

    add .*

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -17,11 +17,12 @@ class CrossHttp
     {
 
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', 'http://127.0.0.1');
+        $response->header('Access-Control-Allow-Origin', '*');
 //        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Access-Token, Cookie, Accept, multipart/form-data, application/json');
         $response->header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        $request->header('Access-Control-Allow-Credentials', 'false');
+        $response->header('Access-Control-Allow-Credentials', 'false');
+        
 
 
         return $response;
