92181f737263f91e5c4021efea58a13f32b3098c
longjiang <375638309@qq.com>
2017-12-12 15:42:42 +0800

    add sms log

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -9,18 +9,18 @@ class CrossHttp
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
 
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', 'http://127.0.0.1');
+        $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        // $response->header('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Allow-Credentials', 'true');
 
         return $response;
 
